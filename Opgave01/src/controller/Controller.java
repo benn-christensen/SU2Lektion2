fb280045dba883da1d257f0416440705f851ab47
Benn Christensen <bnch@eaaa.dk>
2024-02-14 09:30:36 +0100

    more

--- Opgave01/src/controller/Controller.java
+++ Opgave01/src/controller/Controller.java
@@ -1,7 +1,12 @@
 package controller;
 public class Controller {
     public static void main(String[] args) {
+        Vare piratos = new Vare("Piratos", 0.25);
+        Vare skumbanan = new Vare("Skumbanan", 2);
         Order order = new Order();
+        order.tilføjOrderlinje(piratos, 20);
+        order.tilføjOrderlinje(skumbanan, 10);
+        order.beregnSamletPris();
 
     }
 }
